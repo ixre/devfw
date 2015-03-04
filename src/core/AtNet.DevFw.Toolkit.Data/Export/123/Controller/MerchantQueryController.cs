b34ddd1042f215495fe732d0f38e79f7bdc82e35
newmin <new.min@msn.com>
2015-03-04 17:56:53 +0800

    core

---
+++ src/core/AtNet.DevFw.Toolkit.Data/Export/123/Controller/MerchantQueryController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using U1city.ClientCommon;
+using U1city.Common;
+using U1City.Infrastructure.DataExport;
+using U1City.Infrastructure.Extensions;
+using U1City.UPos.Server.Contract.Order;
+using U1City.UPos.Server.Contract.Shop;
+using U1City.UPos.Server.DataTransferObject.Order;
+using U1City.UPos.Server.UI.QueryAndExport.Order;
+using U1City.UPos.Server.UI.QueryAndExport.ToolKit;
+
+namespace U1City.UPos.Server.UI.Controllers
+{
+    public class MerchantQueryController : BaseController
+    {
+        //
+        // GET: /MerchantQuery/
+
+        public ActionResult OfflineShopSaleOrdersQuery()
+        {
+            PagePars pars = new PagePars();
+            pars.Add("MerchantID", base.LoginUserManager.CurrentUserInfo.MerchantID);
+            ViewBag.Shops = new Client<IOfflineShopContract>().GetInstance().GetShopsOfMerchant(pars);
+
+            return View();
+        }
+
+
+        public ActionResult ShowOrderDetail(string orderNo)
+        {
+            long merchantId = base.LoginUserManager.CurrentUserInfo.MerchantID;
+            OrderDTO order = new Client<IOrderContract>().GetInstance().GetOrderByOrderNo(merchantId, orderNo);
+
+            ViewBag.order = order;
+
+            ViewBag.Json = new U1Json();
+            return View();
+        }
+    }
+}
---
+++ src/core/AtNet.DevFw.Toolkit.Data/Export/123/QueryAndExport/Order/OrderExport.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using U1city.ClientCommon;
+using U1City.Infrastructure.DataExport;
+using U1city.UiHelp.Mvc;
+using U1City.UPos.Server.Contract.Common;
+using U1City.UPos.Server.UI.Supports.Security;
+
+namespace U1City.UPos.Server.UI.QueryAndExport.Order
+{
+    public class OrderExport:BaseDataExportPortal
+    {
+        private const string queryName = "Query_MerchantOfflineShopSalesOrder";
+
+        private static string[] columns = new[] { "订单号", "门店名称", "销售日期", "销售时间", "销售数量" };
+
+
+        public override string[] ColumnNames
+        {
+            get { return columns; }
+        }
+
+
+        public override DataTable GetShemalAndData()
+        {
+            return DataService.Instance.GetQueryView(
+                queryName,
+                this.Parameters,
+                10000,
+                1);
+        }
+
+        public override DataRow GetTotalView()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
