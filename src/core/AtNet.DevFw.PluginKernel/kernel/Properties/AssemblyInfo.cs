c0b49f3b94a38a4c5073f5e0d40b2363568ff526
newmin <new.min@msn.com>
2015-03-05 17:22:25 +0800

    commit

---
+++ src/core/AtNet.DevFw.PluginKernel/kernel/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("C#插件内核")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.ops.cc")]
+[assembly: AssemblyProduct("Com.PluginKernel")]
+[assembly: AssemblyCopyright("Copyright © OPS  2010-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b833a605-33e5-4520-a1fd-d94ee603c0b9")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
