c704ba362c80915a6de77bb81bbc39f6c671f6b1
jarrysix <jarrysix@gmail.com>
2017-09-09 18:10:56 +0800

    load font famliy

--- src/core/J6.DevFw.Core/Utils/Utils.cs
+++ src/core/J6.DevFw.Core/Utils/Utils.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Text;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace JR.DevFw.Utils
 {
@@ -21,14 +18,26 @@ namespace JR.DevFw.Utils
         /// <returns></returns>
         public static FontFamily LoadFontFamily(String path)
         {
-            FileStream stream = new FileStream(path, FileMode.Open);
-            int fontSize = (int)stream.Length;
-            byte[] data = new byte[fontSize];
-            stream.Read(data, 0, fontSize);
+            /*
+            FileStream fs = new FileStream(path,FileMode.Open);
+            int fontSize = (int)fs.Length;
+            byte[] fontData = new byte[fontSize];
+            fs.Read(fontData, 0, fontSize);
+            PrivateFontCollection _fonts = new PrivateFontCollection();
+            IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length);
+            Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            _fonts.AddMemoryFont(fontPtr, fontData.Length);
+            Marshal.FreeCoTaskMem(fontPtr);
+            return _fonts.Families[0];
+             */
             PrivateFontCollection fc = new PrivateFontCollection();
-            IntPtr pointer = Marshal.UnsafeAddrOfPinnedArrayElement(data, 0);
-            fc.AddMemoryFont(pointer, fontSize);
-            return fc.Families[0];
+            fc.AddFontFile(path);
+            if(fc.Families.Length > 0)
+            {
+                return fc.Families[0];
+            }
+            return null;
+           
         }
     }
 }
