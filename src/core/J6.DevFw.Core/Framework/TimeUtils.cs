caf8732736acb13193ba1b4578b8f8df10a45af6
刘铭 <jarrysix@gmail.com>
2018-06-29 22:17:54 +0800

    时间戳工具类

---
+++ src/core/J6.DevFw.Core/Framework/TimeUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace JR.DevFw.Framework
+{
+    /// <summary>
+    /// 时间工具类
+    /// </summary>
+    public static class TimeUtils
+    {
+        /// <summary>
+        /// 小时->秒
+        /// </summary>
+        public static int Hour = 3600;
+        /// <summary>
+        /// 小时->秒
+        /// </summary>
+        public static int Day = 24 * Hour;
+
+        private static DateTime unixVar = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        /// <summary>  
+        /// 获取时间戳  
+        /// </summary>  
+        /// <returns></returns>  
+        public static string Unix(DateTime d)
+        {
+            TimeSpan ts = d - unixVar;
+            return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+
+        /// <summary>
+        /// 获取时间戳(微秒)
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static string MilliUnix(DateTime d)
+        {
+            TimeSpan ts = d - unixVar;
+            return Convert.ToInt64(ts.Milliseconds).ToString();
+        }
+
+        /// <summary>
+        /// 获取日期00:00:00的时间戳
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static String DateUnix(DateTime d)
+        {
+            return Unix(d.Date);
+        }
+    }
+}
---
+++ src/test/JR.DevFw.UnitTest/ApiTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JR.DevFw.Framework.Api;
+using JR.DevFw.Framework.Net;
+
+namespace JR.DevFw.UnitTest
+{
+    /// <summary>
+    /// ApiTest 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class ApiTest
+    {
+        public static string OwnerCode = "8deecad7d5720169j96de0ps07d69010";
+        public static string Url = "http://localhost:1419";
+        public static String ApiKey = "6002097906324308";
+        public static String ApiSecret = "306c4fkbsad1a124da7d7ee7ac632a33";
+        public static String SignType = "sha1";
+
+        public ApiTest()
+        {
+            //
+            //TODO:  在此处添加构造函数逻辑
+            //
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDictionary<String, String> dataMap = new Dictionary<String, String>();
+            dataMap["user"] = ApiKey;
+            dataMap["sys"] = "zunxin-sys-001";
+            dataMap["sign_type"] = SignType;
+            String sign= ApiUtils.Sign(SignType, dataMap, ApiSecret);
+            dataMap["sign"] = sign;
+            String query = HttpClient.ParseQuery(dataMap);
+            String url = String.Format("{0}/own/login_gw?{1}", Url, query);
+            Console.WriteLine("快速登录链接为：" + url);
+        }
+    }
+}
