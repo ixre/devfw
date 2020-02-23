0b1a29dd0f88978466355aad1c245040229df2cb
jarry <jarrysix@gmail.com>
2020-02-23 17:30:45 +0800

    standard version

---
+++ src/JR.Stand.Core/Framework/Web/HttpContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace JR.Stand.Core.Framework.Web
+{
+    public static class HttpContext
+
+    {
+        private static IHttpContextAccessor _accessor;
+
+
+        public static Microsoft.AspNetCore.Http.HttpContext Current => _accessor.HttpContext;
+
+
+        public static void Configure(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+    }
+}
