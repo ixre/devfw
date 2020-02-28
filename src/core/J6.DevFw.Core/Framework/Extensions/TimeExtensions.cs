3fb150b74e03d94bc420dc45c8b36a7c4f2ca87b
jarry <jarrysix@gmail.com>
2020-02-28 21:50:08 +0800

    DevFw

---
+++ src/core/J6.DevFw.Core/Framework/Extensions/TimeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using JR.DevFw.Framework;
+
+namespace System
+{
+    /// <summary>
+    /// 时间扩展
+    /// </summary>
+    public static class TimeExtensions
+    {
+        /// <summary>  
+        /// 获取时间戳  
+        /// </summary>  
+        /// <returns></returns>  
+        public static long Unix(this DateTime d)
+        {
+            return TimeUtils.Unix(d);
+        }
+        /// <summary>
+        /// 获取时间戳(微秒)
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static long MilliUnix(DateTime d)
+        {
+            return TimeUtils.MilliUnix(d);
+        }
+
+        /// <summary>
+        /// 获取日期00:00:00的时间戳
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static long DateUnix(DateTime d)
+        {
+            return TimeUtils.DateUnix(d);
+        }
+    }
+}
